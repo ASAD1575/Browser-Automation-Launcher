aeb690e41f3df1e5c38215edae352bf9d6cb86ab
ASAD1575 <masadsubhani8@gmail.com>
2025-11-24 17:39:11 +0500

    terraform errors fixed

--- Iac/terraform/modules/IAM_role/outputs.tf
+++ Iac/terraform/modules/IAM_role/outputs.tf
@@ -1,8 +1,4 @@
 output "instance_profile_name" {
   description = "The name of the IAM instance profile"
-  value = var.create_instance_profile_if_missing ? (
-    aws_iam_instance_profile.instance_profile[0].name
-  ) : (
-    data.aws_iam_instance_profile.existing_profile[0].name
-  )
+  value       = data.aws_iam_instance_profile.existing_profile.name
 }
--- Iac/terraform/modules/IAM_role/variables.tf
+++ Iac/terraform/modules/IAM_role/variables.tf
@@ -2,15 +2,3 @@ variable "existing_role_name" {
   description = "Name of the existing IAM role"
   type        = string
 }
-
-variable "instance_profile_name" {
-  description = "Name of the IAM instance profile (if different from role name). If not specified, uses role name."
-  type        = string
-  default     = null
-}
-
-variable "create_instance_profile_if_missing" {
-  description = "If true, create the instance profile if it doesn't exist. If false, only use existing instance profile."
-  type        = bool
-  default     = true
-}
