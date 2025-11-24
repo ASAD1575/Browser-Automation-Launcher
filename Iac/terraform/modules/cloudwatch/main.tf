ac4f450d8eaa3bbe3d829d2f456a39631b2b4603
ASAD1575 <masadsubhani8@gmail.com>
2025-11-24 18:07:34 +0500

    workflow fixed

--- Iac/terraform/modules/cloudwatch/main.tf
+++ Iac/terraform/modules/cloudwatch/main.tf
@@ -2,7 +2,7 @@ resource "aws_cloudwatch_log_group" "app" {
   name              = var.log_group_name
   retention_in_days = var.retention_in_days
   kms_key_id        = var.cw_kms_key_arn
-  tags              = { Environment = "prod" }
+  tags              = { Environment = "dev" }
 }
 
 resource "aws_ssm_parameter" "cwagent_windows" {
@@ -10,7 +10,7 @@ resource "aws_ssm_parameter" "cwagent_windows" {
   type  = "String"
   tier  = "Standard"
   value = var.cwagent_config_json
-  tags  = { Environment = "prod" }
+  tags  = { Environment = "dev" }
 }
 
 
